69e6f8bd3caf6eb43ccacfeff5a46d4e7566fa71
facundotourn <cchori@hotmail.com.ar>
2017-07-07 20:46:16 -0300

    Clase Pinche terminada

--- Pinche.js
+++ Pinche.js
@@ -3,11 +3,70 @@ function Pinche(x, y, orientacion) {
   this.y = y;
   this.orientacion = orientacion;
   this.arista = 40;
+  this.visible = false;
 
-  this.show = function() {
+  this.p1 = [this.x, this.y + this.arista * 0.5];
+  this.p2 = [this.x, this.y - this.arista * 0.5];
+  this.p3 = [this.x - this.arista * 0.7, this.y];
+
+
+  this.draw = function() {
     fill(255);
     noStroke();
-    triangle(this.x, this.y + this.arista * 0.5, this.x, this.y - this.arista * 0.5, this.x - this.arista * 0.7, this.y);
+    triangle(this.p1[0], this.p1[1], this.p2[0], this.p2[1], this.p3[0], this.p3[1]);
   }
 
+  this.update = function() {
+    if (!this.visible && this.orientacion[1] == 0) {
+      if(this.orientacion[0] == 1) {
+        // Es un pinche de la izquierda
+        if(this.x > x - this.arista * 0.7) {
+          this.x -= 3;
+          this.calcularPuntos();
+        }
+      } else if(this.orientacion[0] == -1) {
+        // Es un pinche de la derecha
+        if(this.x < x + this.arista) {
+          this.x += 3;
+          this.calcularPuntos();
+        }
+      }
+    } else if(this.visible && this.orientacion[1] == 0) {
+      if(this.orientacion[0] == 1) {
+        // Es un pinche de la izquierda
+        if(this.x < 0) {
+          this.x += 3;
+          this.calcularPuntos();
+        }
+      } else if(this.orientacion[0] == -1) {
+        // Es un pinche de la derecha
+        if(this.x > x) {
+          this.x -= 3;
+          this.calcularPuntos();
+        }
+      }
+    }
+  }
+
+  this.ocultar = function() {
+    this.visible = false;
+  }
+
+  this.mostrar = function() {
+    this.visible = true;
+  }
+
+  this.calcularPuntos = function() {
+    if(orientacion[1] == 0) {
+      if(orientacion[0] == 1) {
+        this.p1 = [this.x, this.y + this.arista * 0.5];
+        this.p2 = [this.x, this.y - this.arista * 0.5];
+        this.p3 = [this.x + this.arista * 0.7, this.y];
+      } else {
+        this.p1 = [this.x, this.y + this.arista * 0.5];
+        this.p2 = [this.x, this.y - this.arista * 0.5];
+        this.p3 = [this.x - this.arista * 0.7, this.y];
+      }
+    }
+  }
 }
--- Spikes.js
+++ Spikes.js
@@ -1,10 +1,13 @@
 var pajaro;
 var pinche;
+var pinche2;
 var gravedad = 0.35;
 
 function setup() {
   createCanvas(480, 800);
   pajaro = new Pajaro(width / 2, height / 4);
+  pinche = new Pinche(width, height / 4, [-1, 0]);
+  pinche2 = new Pinche(0, height / 4, [1, 0]);
 }
 
 function draw() {
@@ -13,11 +16,24 @@ function draw() {
 
   pajaro.show();
   pajaro.update();
+
+  pinche.draw();
+  pinche.update();
+  pinche2.draw();
+  pinche2.update();
 }
 
 function mousePressed() {
   if(pajaro.mov) {
     gameOver();
+  }
+
+  if(pinche.visible) {
+    pinche.ocultar();
+    pinche2.ocultar();
+  } else {
+    pinche.mostrar();
+    pinche2.mostrar();
   }
 }
 
@@ -25,6 +41,13 @@ function keyPressed() {
   if(key === ' ') {
     pajaro.jump();
     pajaro.mov = true;
+  }
+
+  if(key == 'a') {
+    if(pinche.visible)
+      pinche.hide();
+    else
+      pinche.show();
   }
 }
 
